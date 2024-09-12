e2b43780422f8b7e49dd9eb3dc15d3dc069b3d42
gsampaiowz <gabrielsampaio1216@gmail.com>
2024-09-12 17:24:48 -0300

    infinite viewer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
-import { useState } from "react";
 import dynamic from "next/dynamic";
 import { CanvasPage } from "@/components/canvasPage";
 import { Button } from "@/components/ui/button";
+import InfiniteViewer from "react-infinite-viewer";
 
 const Canvas = dynamic(() => import("../components/myCanvas"), {
   ssr: false,
@@ -10,18 +10,24 @@ const Canvas = dynamic(() => import("../components/myCanvas"), {
 
 const HomePage: React.FC = () => {
   return (
-    <Canvas>
-      <main className="flex flex-col gap-24 justify-center items-center h-screen w-screen">
-        <div className="flex gap-20">
-          <CanvasPage />
-          <CanvasPage />
-          <CanvasPage />
-        </div>
-        <div className="py-2 px-64 bg-gray-200 border border-gray-400 rounded-lg">
-          <Button className="py-6 px-12 bg-gray-500">+ Nova página</Button>
-        </div>
-      </main>
-    </Canvas>
+    <InfiniteViewer
+      className="viewer size-full"
+    >
+      <div className="viewport">
+        <main className="flex flex-col gap-24 justify-center items-center h-screen w-screen">
+          {/* <Canvas> */}
+          <div className="flex gap-20">
+            <CanvasPage />
+            <CanvasPage />
+            <CanvasPage />
+          </div>
+          <div className="py-2 px-64 bg-gray-200 border border-gray-400 rounded-lg">
+            <Button className="py-6 px-12 bg-gray-500">+ Nova página</Button>
+          </div>
+          {/* </Canvas> */}
+        </main>
+      </div>
+    </InfiniteViewer>
   );
 };
 
