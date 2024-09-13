bd34bc6732ebc387789a57c418852b56e0f20d3e
gsampaiowz <gabrielsampaio1216@gmail.com>
2024-09-13 17:26:19 -0300

    compreendendo interações

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,72 @@
 "use client";
-import dynamic from "next/dynamic";
 import { CanvasPage } from "@/components/canvasPage";
 import { Button } from "@/components/ui/button";
-import InfiniteViewer from "react-infinite-viewer";
-import { useRef, useState } from "react";
-
-const Canvas = dynamic(() => import("../components/myCanvas"), {
-  ssr: false,
-});
+import { Trash2 } from "lucide-react";
+import { useRef } from "react";
+import InfiniteViewer, { InfiniteViewerProps } from "react-infinite-viewer";
+import { Group, Layer, Rect, Stage, Text, Shape } from "react-konva";
+import { Html } from "react-konva-utils";
 
 const HomePage: React.FC = () => {
-  const [visible, setVisible] = useState(true);
+  const viewer = useRef<InfiniteViewerProps>(null);
+
+  requestAnimationFrame(() => {
+    viewer.current!.scrollCenter();
+  });
 
   return (
-    <InfiniteViewer
-      className="!w-screen !h-screen"
-      displayHorizontalScroll={visible}
-      preventWheelClick
-      displayVerticalScroll={visible}
-    >
-      <div className="">
-        <main className="flex flex-col gap-24 justify-center items-center h-screen w-screen">
-          {/* <Canvas> */}
-          <div className="flex gap-20">
-            <CanvasPage />
-            <CanvasPage />
-            <CanvasPage />
-          </div>
-          <div className="py-2 px-64 bg-gray-200 border border-gray-400 rounded-lg">
-            <Button className="py-6 px-12 bg-gray-500">+ Nova página</Button>
-          </div>
-          {/* </Canvas> */}
-        </main>
-      </div>
-    </InfiniteViewer>
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer width={window.innerWidth} height={window.innerHeight}>
+        {/* <Html> */}
+        {/* <InfiniteViewer
+            ref={viewer as any}
+            id="viewer"
+            className="!w-screen !h-screen"
+            useMouseDrag={true}
+            useWheelScroll={true}
+            useAutoZoom={true}
+            zoomRange={[0.1, 10]}
+            maxPinchWheel={10}
+            onDragStart={(e) => {
+              const target = e.inputEvent.target;
+
+              if (target.nodeName === "A") {
+                e.stop();
+              }
+            }}
+          > */}
+        {/* <div className="wrap">
+              <main className="flex flex-col gap-12 justify-center items-center h-screen w-screen"> */}
+        {/* <Canvas> */}
+        {/* <div className="flex gap-20">
+                  <CanvasPage />
+                  <CanvasPage />
+                  <CanvasPage />
+                </div>
+                <div className="fixed bottom-5 left-1/2 -translate-x-1/2 py-2 px-64 bg-gray-200 border border-gray-400 rounded-lg">
+                  <Button className="py-6 px-12 bg-gray-500">
+                    + Nova página
+                  </Button>
+                </div> */}
+        <Group draggable>
+          <Rect width={200} height={100} fill={"#b3b3b3"} cornerRadius={10} />
+          {/* <Text x={30} y={40} text="TESTE" fill={"#fff"} />
+          <Group x={140} y={32}> */}
+          <Html transform={true}>
+            <div className="flex-1 flex justify-between items-center py-6 bg-gray-400 gap-4 rounded px-3">
+              <p>teste</p>
+              <Trash2 className="bg-gray-500 fill-gray-400 text-gray-600 mb-2 self-end p-1 rounded size-7 cursor-pointer" />
+            </div>{" "}
+          </Html>
+        </Group>
+        {/* </Group> */}
+        {/* </Canvas> */}
+        {/* </main>
+            </div> */}
+        {/* </InfiniteViewer> */}
+        {/* </Html> */}
+      </Layer>
+    </Stage>
   );
 };
 
--- src/components/canvasPage.tsx
+++ src/components/canvasPage.tsx
@@ -2,19 +2,23 @@ import React from "react";
 import { Button } from "./ui/button";
 import { PageItem } from "./pageItem";
 import { Trash2 } from "lucide-react";
-import { Layer, Rect, Stage } from "react-konva";
+import { Group, Layer, Rect, Stage } from "react-konva";
 
 export const CanvasPage = () => {
   return (
-    <div className="flex flex-col">
-      <Trash2 className="bg-gray-300 fill-gray-400 text-gray-500 mb-2 self-end p-1 rounded size-7 cursor-pointer" />
-      <div className="p-4 flex flex-col gap-4 bg-gray-300 w-56">
-        <PageItem text="teste" />
-        <PageItem text="teste" />
-        <PageItem text="teste" />
-        <PageItem text="teste" />
+    <Group draggable>
+      <div className="flex flex-col p-4 bg-gray-300 rounded">
+        <Trash2 className="bg-gray-300 fill-gray-400 text-gray-500 mb-2 self-end p-1 rounded size-7 cursor-pointer" />
+        <div className="flex flex-col gap-4 w-56">
+          <PageItem text="teste" />
+          <PageItem text="teste" />
+          <PageItem text="teste" />
+          <PageItem text="teste" />
+        </div>
+        <Button className="bg-transparent mt-6 rounded-none border border-dashed border-gray-500 text-gray-500 hover:bg-gray-200">
+          + Novo item
+        </Button>
       </div>
-      <Button className="bg-transparent mt-6 rounded-none border border-dashed border-gray-500 text-gray-500 hover:bg-gray-200">+ Novo item</Button>
-    </div>
+    </Group>
   );
 };
