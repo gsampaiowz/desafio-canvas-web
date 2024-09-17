144bc8e36875860e891638a8cedcbc6c3f024b76
gsampaiowz <gabrielsampaio1216@gmail.com>
2024-09-17 12:31:27 -0300

    começando novamente

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,34 @@
 "use client";
-import { CanvasPage } from "@/components/canvasPage";
+import { DraggablePage } from "@/components/draggablePage";
 import { PageItem } from "@/components/pageItem";
 import { Button } from "@/components/ui/button";
-import {
-  closestCorners,
-  DndContext,
-  DragEndEvent,
-  DragMoveEvent,
-  DragStartEvent,
-  KeyboardSensor,
-  PointerSensor,
-  UniqueIdentifier,
-  useSensor,
-  useSensors,
-} from "@dnd-kit/core";
-import {
-  arrayMove,
-  SortableContext,
-  sortableKeyboardCoordinates,
-} from "@dnd-kit/sortable";
-import { useRef, useState } from "react";
-import InfiniteViewer, { InfiniteViewerProps } from "react-infinite-viewer";
-import { v4 as uuidv4 } from "uuid";
+import { useState } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { v4 as uuid } from "uuid";
 
-type DNDType = {
-  id: UniqueIdentifier;
+type PageProps = {
+  id: string;
   items: {
-    id: UniqueIdentifier;
+    id: string;
     title: string;
   }[];
 };
 
-const HomePage: React.FC = () => {
-  // const viewer = useRef<InfiniteViewerProps>(null);
+const Desafio: React.FC = () => {
+  const [pages, setPages] = useState<PageProps[]>([
+    {
+      id: uuid(),
+      items: [
+        {
+          id: uuid(),
+          title: "Item 1",
+        },
+      ],
+    },
+  ]);
 
-  const [pages, setPages] = useState<DNDType[]>([]);
-  const [activeId, setActiveId] = useState<UniqueIdentifier | null>(null);
-  const [currentPageId, setCurrentPageId] = useState<UniqueIdentifier>();
-
-  const onAddPage = () => {
-    const id = `container-${uuidv4()}`;
+  const AddPage = () => {
+    const id = `container-${uuid()}`;
     setPages([
       ...pages,
       {
@@ -49,277 +38,23 @@ const HomePage: React.FC = () => {
     ]);
   };
 
-  const onAddItem = (pageId: UniqueIdentifier) => {
-    console.log("teste");
-    const id = `item-${uuidv4()}`;
+  const AddItem = (pageId: string) => {
+    const id = `item-${uuid()}`;
     const page = pages.find((item) => item.id === pageId);
-    console.log("teste");
+
     if (!page) return;
 
     page.items.push({
       id,
-      title: "teste",
+      title: "Item " + (page.items.length + 1),
     });
     setPages([...pages]);
   };
-
-  // Find the value of the items
-  function findValueOfItems(id: UniqueIdentifier | undefined, type: string) {
-    if (type === "container") {
-      return pages.find((item) => item.id === id);
-    }
-    if (type === "item") {
-      return pages.find((container) =>
-        container.items.find((item) => item.id === id)
-      );
-    }
-  }
-
-  const findItemTitle = (id: UniqueIdentifier | undefined) => {
-    const container = findValueOfItems(id, "item");
-    if (!container) return "";
-    const item = container.items.find((item) => item.id === id);
-    if (!item) return "";
-    return item.title;
-  };
-
-  const findPageItems = (id: UniqueIdentifier | undefined) => {
-    const container = findValueOfItems(id, "container");
-    if (!container) return [];
-    return container.items;
-  };
-
-  const sensors = useSensors(
-    useSensor(PointerSensor),
-    useSensor(KeyboardSensor, {
-      coordinateGetter: sortableKeyboardCoordinates,
-    })
-  );
-
-  function handleDragStart(event: DragStartEvent) {
-    const { active } = event;
-    const { id } = active;
-    setActiveId(id);
-  }
-
-  const handleDragMove = (event: DragMoveEvent) => {
-    const { active, over } = event;
-
-    // Handle Items Sorting
-    if (
-      active.id.toString().includes("item") &&
-      over?.id.toString().includes("item") &&
-      active &&
-      over &&
-      active.id !== over.id
-    ) {
-      // Find the active container and over container
-      const activeContainer = findValueOfItems(active.id, "item");
-      const overContainer = findValueOfItems(over.id, "item");
-
-      // If the active or over container is not found, return
-      if (!activeContainer || !overContainer) return;
-
-      // Find the index of the active and over container
-      const activeContainerIndex = pages.findIndex(
-        (container) => container.id === activeContainer.id
-      );
-      const overContainerIndex = pages.findIndex(
-        (container) => container.id === overContainer.id
-      );
-
-      // Find the index of the active and over item
-      const activeitemIndex = activeContainer.items.findIndex(
-        (item) => item.id === active.id
-      );
-      const overitemIndex = overContainer.items.findIndex(
-        (item) => item.id === over.id
-      );
-      // In the same container
-      if (activeContainerIndex === overContainerIndex) {
-        let newItems = [...pages];
-        newItems[activeContainerIndex].items = arrayMove(
-          newItems[activeContainerIndex].items,
-          activeitemIndex,
-          overitemIndex
-        );
-
-        setPages(newItems);
-      } else {
-        // In different pages
-        let newItems = [...pages];
-        const [removeditem] = newItems[activeContainerIndex].items.splice(
-          activeitemIndex,
-          1
-        );
-        newItems[overContainerIndex].items.splice(
-          overitemIndex,
-          0,
-          removeditem
-        );
-        setPages(newItems);
-      }
-    }
-
-    // Handling Item Drop Into a Container
-    if (
-      active.id.toString().includes("item") &&
-      over?.id.toString().includes("container") &&
-      active &&
-      over &&
-      active.id !== over.id
-    ) {
-      // Find the active and over container
-      const activeContainer = findValueOfItems(active.id, "item");
-      const overContainer = findValueOfItems(over.id, "container");
-
-      // If the active or over container is not found, return
-      if (!activeContainer || !overContainer) return;
-
-      // Find the index of the active and over container
-      const activeContainerIndex = pages.findIndex(
-        (container) => container.id === activeContainer.id
-      );
-      const overContainerIndex = pages.findIndex(
-        (container) => container.id === overContainer.id
-      );
-
-      // Find the index of the active and over item
-      const activeitemIndex = activeContainer.items.findIndex(
-        (item) => item.id === active.id
-      );
-
-      // Remove the active item from the active container and add it to the over container
-      let newItems = [...pages];
-      const [removeditem] = newItems[activeContainerIndex].items.splice(
-        activeitemIndex,
-        1
-      );
-      newItems[overContainerIndex].items.push(removeditem);
-      setPages(newItems);
-    }
-  };
-
-  // This is the function that handles the sorting of the pages and items when the user is done dragging.
-  function handleDragEnd(event: DragEndEvent) {
-    const { active, over } = event;
-
-    // Handling Container Sorting
-    if (
-      active.id.toString().includes("container") &&
-      over?.id.toString().includes("container") &&
-      active &&
-      over &&
-      active.id !== over.id
-    ) {
-      // Find the index of the active and over container
-      const activeContainerIndex = pages.findIndex(
-        (container) => container.id === active.id
-      );
-      const overContainerIndex = pages.findIndex(
-        (container) => container.id === over.id
-      );
-      // Swap the active and over container
-      let newItems = [...pages];
-      newItems = arrayMove(newItems, activeContainerIndex, overContainerIndex);
-      setPages(newItems);
-    }
-
-    // Handling item Sorting
-    if (
-      active.id.toString().includes("item") &&
-      over?.id.toString().includes("item") &&
-      active &&
-      over &&
-      active.id !== over.id
-    ) {
-      // Find the active and over container
-      const activeContainer = findValueOfItems(active.id, "item");
-      const overContainer = findValueOfItems(over.id, "item");
-
-      // If the active or over container is not found, return
-      if (!activeContainer || !overContainer) return;
-      // Find the index of the active and over container
-      const activeContainerIndex = pages.findIndex(
-        (container) => container.id === activeContainer.id
-      );
-      const overContainerIndex = pages.findIndex(
-        (container) => container.id === overContainer.id
-      );
-      // Find the index of the active and over item
-      const activeitemIndex = activeContainer.items.findIndex(
-        (item) => item.id === active.id
-      );
-      const overitemIndex = overContainer.items.findIndex(
-        (item) => item.id === over.id
-      );
-
-      // In the same container
-      if (activeContainerIndex === overContainerIndex) {
-        let newItems = [...pages];
-        newItems[activeContainerIndex].items = arrayMove(
-          newItems[activeContainerIndex].items,
-          activeitemIndex,
-          overitemIndex
-        );
-        setPages(newItems);
-      } else {
-        // In different pages
-        let newItems = [...pages];
-        const [removeditem] = newItems[activeContainerIndex].items.splice(
-          activeitemIndex,
-          1
-        );
-        newItems[overContainerIndex].items.splice(
-          overitemIndex,
-          0,
-          removeditem
-        );
-        setPages(newItems);
-      }
-    }
-    // Handling item dropping into Container
-    if (
-      active.id.toString().includes("item") &&
-      over?.id.toString().includes("container") &&
-      active &&
-      over &&
-      active.id !== over.id
-    ) {
-      // Find the active and over container
-      const activeContainer = findValueOfItems(active.id, "item");
-      const overContainer = findValueOfItems(over.id, "container");
-
-      // If the active or over container is not found, return
-      if (!activeContainer || !overContainer) return;
-      // Find the index of the active and over container
-      const activeContainerIndex = pages.findIndex(
-        (container) => container.id === activeContainer.id
-      );
-      const overContainerIndex = pages.findIndex(
-        (container) => container.id === overContainer.id
-      );
-      // Find the index of the active and over item
-      const activeitemIndex = activeContainer.items.findIndex(
-        (item) => item.id === active.id
-      );
-
-      let newItems = [...pages];
-      const [removeditem] = newItems[activeContainerIndex].items.splice(
-        activeitemIndex,
-        1
-      );
-      newItems[overContainerIndex].items.push(removeditem);
-      setPages(newItems);
-    }
-    setActiveId(null);
-  }
 
   return (
     // <InfiniteViewer
     // displayVerticalScroll={false}
     // displayHorizontalScroll={false}
-    // ref={viewer as any}
     // className="!w-screen !h-screen"
     // useMouseDrag={true}
     // useWheelScroll={true}
@@ -335,34 +70,8 @@ const HomePage: React.FC = () => {
     // }}
     // >
     <main className="flex justify-center items-center h-screen w-screen">
-      <DndContext
-        sensors={sensors}
-        collisionDetection={closestCorners}
-        onDragStart={handleDragStart}
-        onDragEnd={handleDragEnd}
-        onDragMove={handleDragMove}
-      >
-        <SortableContext items={pages.map((i) => i.id)}>
-          {pages.map((page, index) => (
-            <CanvasPage
-              id={page.id}
-              onAddItem={() => onAddItem(page.id)}
-              key={index}
-            >
-              <SortableContext items={page.items.map((i) => i.id)}>
-                <div className="flex flex-col gap-4 w-56">
-                  {page.items.map((i) => (
-                    <PageItem title={i.title} id={i.id} key={i.id} />
-                  ))}
-                </div>
-              </SortableContext>
-            </CanvasPage>
-          ))}
-        </SortableContext>
-      </DndContext>
-
       <div className="fixed bottom-5 left-1/2 -translate-x-1/2 py-2 px-64 bg-neutral-200 border border-neutral-400 rounded-lg">
-        <Button onClick={onAddPage} className="py-6 px-12 bg-neutral-500">
+        <Button onClick={AddPage} className="py-6 px-12 bg-neutral-500">
           + Nova página
         </Button>
       </div>
@@ -371,4 +80,4 @@ const HomePage: React.FC = () => {
   );
 };
 
-export default HomePage;
+export default Desafio;
--- src/components/pageItem.tsx
+++ src/components/pageItem.tsx
@@ -1,32 +1,15 @@
-import { UniqueIdentifier, useDraggable } from "@dnd-kit/core";
 import { Trash2 } from "lucide-react";
 import React from "react";
 
 interface PageItemProps {
-  id: UniqueIdentifier;
   title: string;
 }
 
-export const PageItem = ({ id, title }: PageItemProps) => {
-  const { attributes, listeners, setNodeRef, transform } = useDraggable({
-    id: "draggable",
-  });
-  const style = transform
-    ? {
-        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
-      }
-    : undefined;
-
+export const PageItem = ({ title }: PageItemProps) => {
   return (
-    <div
-      ref={setNodeRef}
-      style={style}
-      {...listeners}
-      {...attributes}
-      className="flex-1 flex justify-between items-center py-6 bg-neutral-400 gap-4 rounded px-3 relative z-50"
-    >
+    <div className="flex-1 flex justify-between items-center py-6 bg-neutral-400 gap-4 rounded px-3 relative z-50">
       <p>{title}</p>
-      <Trash2 className="bg-neutral-500 fill-neutral-400 text-neutral-600 mb-2 self-end p-1 rounded size-7 cursor-pointer" />
+      <Trash2 className="bg-neutral-500 fill-neutral-400 text-neutral-600 self-end p-1 rounded size-7 cursor-pointer" />
     </div>
   );
 };
